bdbdac1cfbd0b2464d0db881490d6dc3a4fc0db1
Midle68 <lincrafting@gmail.com>
2022-04-17 20:40:13 +0300

    Small changes

--- main.py
+++ main.py
@@ -9,10 +9,15 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
+import os
+from dotenv import load_dotenv
 
+
+load_dotenv("/Users/midle/Desktop/Hide/hidden.env")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = SECRET_KEY
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
